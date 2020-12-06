048b3f23c12ab9e3f22e4f64c185722d974c6745
keshav0128 <71015755+keshav0128@users.noreply.github.com>
2020-12-06 14:14:11 +0530

    Add files via upload

---
+++ polymerization 1a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class complex
+ {
+     int a,b;
+     public:
+     void set(int x,int y)
+      {
+          a=x;
+          b=y;
+      }
+     void show()
+      {
+          cout<<"a is"<<a<<endl;
+          cout<<"b is"<<b<<endl;
+      }
+      complex add(complex c)
+      {
+          complex temp;
+          temp.a=a+c.a;
+          temp.b=b+c.b;
+          return(temp);
+      }
+ };
+
+int main() {
+    complex c1,c2,c3;
+    c1.set(5,9);
+    c2.set(8,6);
+    c3=c1.add(c2);
+    c3.show();
+	
+	return 0;
+}
